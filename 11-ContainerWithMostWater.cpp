39dabc4c5732820b3bb4930c352342176cb42794
yindex <yzindex@gmail.com>
2020-08-05 21:38:11 +0800

    11 CWMW

---
+++ 11-ContainerWithMostWater.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int left = 0;
+        int right = height.size() - 1;
+        int maxArea = 0;
+        while (left < right) {
+            int area = min(height.at(left), height.at(right)) * (right - left);
+            maxArea = max(maxArea, area);
+            if (height.at(left) <= height.at(right)) left++;
+            else left++;
+        }
+        return maxArea;
+    }
+};
